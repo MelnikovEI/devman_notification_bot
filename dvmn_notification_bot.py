8c7e3cac968c2b72c3a7da28329d003c80c35969
MelnikovEI <MelnikovEI@ya.ru>
2023-04-04 19:17:06 +0300

    decrease repeated func using

--- dvmn_notification_bot.py
+++ dvmn_notification_bot.py
@@ -27,26 +27,27 @@ def main():
             dvmn_response = requests.get("https://dvmn.org/api/long_polling/", headers=headers, timeout=90,
                                          params=params)
             dvmn_response.raise_for_status()
+            dvmn_check_list = dvmn_response.json()
         except requests.exceptions.ReadTimeout:
             continue
         except (requests.exceptions.ConnectionError, requests.exceptions.ConnectTimeout):
             sleep(15)
             continue
-        if dvmn_response.json()['status'] == 'found':
-            params['timestamp'] = dvmn_response.json()['last_attempt_timestamp']
+        if dvmn_check_list['status'] == 'found':
+            params['timestamp'] = dvmn_check_list['last_attempt_timestamp']
             bot.send_message(
                 chat_id=chat_id,
-                text=f'Преподаватель проверил работу "{dvmn_response.json()["new_attempts"][0]["lesson_title"]}".\n'
-                     f'{dvmn_response.json()["new_attempts"][0]["lesson_url"]}'
+                text=f'Преподаватель проверил работу "{dvmn_check_list["new_attempts"][0]["lesson_title"]}".\n'
+                     f'{dvmn_check_list["new_attempts"][0]["lesson_url"]}'
             )
-            if dvmn_response.json()['new_attempts'][0]['is_negative']:
+            if dvmn_check_list['new_attempts'][0]['is_negative']:
                 bot.send_message(chat_id=chat_id, text="К сожалению, в работе нашлись ошибки :(")
             else:
                 bot.send_message(chat_id=chat_id,
                                  text="Преподавателю всё понравилось, можно приступать к следующему уроку!"
                                  )
-        if dvmn_response.json()['status'] == 'timeout':
-            params['timestamp'] = dvmn_response.json()['timestamp_to_request']
+        if dvmn_check_list['status'] == 'timeout':
+            params['timestamp'] = dvmn_check_list['timestamp_to_request']
 
 
 if __name__ == '__main__':
